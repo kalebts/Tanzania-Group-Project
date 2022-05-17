543cddd2d70eafccda4a81280de38d791df96421
Kaleb Tsegaye <kalebts@gmail.com>
2022-05-16 23:10:28 -0400

    changes to streamlit app

--- app-streamlit.py
+++ app-streamlit.py
@@ -1,20 +1,48 @@
 import streamlit as st
 import plotly.express as px
-from os.path import dirname, join
+from os.path import dirname, join, isfile
+from os import listdir
 from streamlit import components
+from streamlit_folium import st_folium
+import folium
 import geopandas as gpd
-
-current_dir = dirname(__file__)
-tz_map_1_path = join(current_dir, "tz_map_1.html")
-tz_map_2_path = join(current_dir, "tz_map_2.html")
+import pandas as pd
 
 st.set_page_config(layout="wide")
 
-st.title('Tanzania Water Pumps Analysis')
-st.header("Click the button to change between maps")
-map_button = st.selectbox('Choose map', ('map 1', 'map 2'))
+# water_service_df = pd.read_csv('tz_dataset') read sb's dataset
 
-if map_button == 'map 1':
-    components.v1.html(open(tz_map_1_path).read(), height=500)
-elif map_button == 'map 2':
-    components.v1.html((open(tz_map_2_path).read()), height=500)
+
+indicators = []
+
+# help from https://www.geeksforgeeks.org/how-to-iterate-over-files-in-directory-using-python/
+current_dir = dirname(__file__)
+for file in listdir(current_dir):
+    f = join(current_dir, file)
+    if isfile(f) and 'tz_map' in file:
+        indicators.append(file)
+        
+# tz_map_1_path = join(current_dir, "tz_map_1.html")
+# tz_map_2_path = join(current_dir, "tz_map_2.html")
+
+# template for maps: tz_map_(name_of_map).html
+# then, split (name_of_map) by underscore.
+
+st.title('Tanzania Water Pumps Analysis')
+
+row1_1, row1_2 = st.columns((4, 5))
+
+
+for i in range(len(indicators)):
+    indicators[i] = " ".join(indicators[i][:-5].split('_')[2:])
+
+indicators = tuple(indicators)
+
+with row1_1:
+    # indicator_button = st.selexctbox('Choose map', ('map 1', 'map 2'))
+    indicator_button = st.selectbox('Choose indicator', indicators)
+
+
+with row1_2:
+    # components.v1.html(open(current_dir+'/tz_map_'+indicator_button+'.html').read(), height=600)
+    streamlit_folium.st_folium
